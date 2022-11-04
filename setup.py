20cca4e4ad57b89968abb13306a94c3280dc976c
Dan Gordon <dan.gordon@anu.edu.au>
2022-11-04 13:29:47 +1100

    Move files into standard python package structure.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='converge-load-forecasting',
+    version='v0.0.1',
+    author='Seyyed Mahdi Noori Rahim Abadi, Dan Gordon',
+    author_email='mahdi.noori@anu.edu.au',
+    packages=['converge_load_forecasting'],
+    install_requires=[
+        'more_itertools',
+        'multiprocess',
+        'pandas',
+        'pyomo',
+        'skforecast=0.4.2',
+        'sklearn'
+    ],
+    scripts=[
+    ]
+)
