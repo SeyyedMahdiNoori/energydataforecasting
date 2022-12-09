64fd5f2ba595ca1c48b81f8ca75d19d98d1d630e
MahdiNoori <mahdi.noori@anu.edu.au>
2022-12-09 15:11:35 +1100

    Add the license file and update gitignore

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 
 from setuptools import setup
 
+with open("README.md" , "r")as fh:
+    long_description = fh.read()
+
 setup(
     name='converge-load-forecasting',
-    version='v0.0.1',
+    version='0.0.3',
     author='Seyyed Mahdi Noori Rahim Abadi, Dan Gordon',
     author_email='mahdi.noori@anu.edu.au',
+    long_description=long_description,
+    long_description_content_type = 'text/markdown',
     packages=['converge_load_forecasting'],
     install_requires=[
         'matplotlib',
