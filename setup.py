090bdba017d1112a536ad2d3c9ef6854bbe83857
MahdiNoori <mahdi.noori@anu.edu.au>
2023-01-30 14:26:42 +1100

    add package connectorx to the setup.py file

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     long_description_content_type = 'text/markdown',
     packages=['converge_load_forecasting'],
     install_requires=[
+        'connectorx',
         'matplotlib',
         'more_itertools',
         'multiprocess',
